481e90ded13fcf2e2776de55444dda8b3cb8b338
Felipe Cano <felipecanol@gmail.com>
2017-02-18 16:45:43 -0500

    Se ajusto el proyecto para usar Maven

--- README.md
+++
@@ -1,2 +0,0 @@
-# precise_estimate_server
-Backend del software para estimación de costos de un proyecto de software
---
+++ src/main/java/com/apps/precise/rest/TestResource.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apps.precise.rest;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author felipe
+ */
+@Path("test")
+public class TestResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of TestResource
+     */
+    public TestResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.apps.precise.rest.TestResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        return "{\"pepe\":1}";
+    }
+
+    /**
+     * PUT method for updating or creating an instance of TestResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+    }
+}
