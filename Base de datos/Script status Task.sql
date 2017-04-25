a0247d8470b17614b9c45791db395529bc39195b
Sao912 <sak8o12@hotmail.com>
2017-04-25 09:56:45 -0500

    Script status task

--- Base de datos/Script status Task.sql
+++ Base de datos/Script status Task.sql
@@ -8,3 +8,18 @@ FOREIGN KEY (`id_status`)
 REFERENCES `precise`.`status` (`id_status`)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
+
+update task
+set id_status = (select id_status from status where status = 'Iniciado')
+where task.name like '%epica%';
+
+update task
+set id_status = (select id_status from status where status = 'Finalizado')
+where task.name like '%proyecto%';
+
+update task
+set id_status =  (select id_status from status where status = 'En Desarrollo')
+where task.id_status is null;
+
+
+
